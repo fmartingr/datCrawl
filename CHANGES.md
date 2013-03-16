c200d477f9032988e63c4fb13f678354802dad7a
Felipe Martin Garcia <fmartingr@me.com>
2013-03-16 16:45:01 +0100

    Fixed PyPi package

--- CHANGES.md
+++ CHANGES.md
@@ -1,2 +1,5 @@
+## 0.1.1 (2013-03-16)
+- Fixing pypi package
+
 ## 0.1.0 (2013-03-16)
 - Initial release
--- setup.py
+++ setup.py
@@ -2,13 +2,11 @@ from distutils.core import setup
 
 setup(
     name='datCrawl',
-    version='0.1.0',
+    version='0.1.1',
     author='Felipe Martin',
     author_email='fmartingr@me.com',
     packages=['datCrawl'],
-    url='http://pypi.python.org/pypi/datCrawl/',
-    git='http://github.com/fmartingr/datCrawl',
+    url='http://github.com/fmartingr/datCrawl/',
     license='LICENSE',
-    description='Crawl as it you were using an API.',
-    long_description=open('README.md').read(),
+    description='Crawl as is you were using an API.',
 )
