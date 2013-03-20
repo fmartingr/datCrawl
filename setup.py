536a0534b81ce63bf679c10feb8113b5b4c4e386
Felipe Martín <fmartingr@me.com>
2013-03-20 12:35:09 +0100

    Release 0.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='datCrawl',
-    version='0.1.1',
+    version='0.2.0',
     author='Felipe Martin',
     author_email='fmartingr@me.com',
     packages=['datCrawl'],
